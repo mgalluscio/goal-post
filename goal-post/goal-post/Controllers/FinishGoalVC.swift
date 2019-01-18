c9352277e144912adfd7435be9d59949a3404a75
Mario Galluscio <mgalluscio@deliverydudes.com>
2019-01-17 21:28:58 -0500

    back button works, added save func to finishgoalVC with completion handler to save goal to core data model, Goal

--- goal-post/goal-post/Controllers/FinishGoalVC.swift
+++ goal-post/goal-post/Controllers/FinishGoalVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class FinishGoalVC: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var createGoalBtn: UIButton!
@@ -25,8 +26,36 @@ class FinishGoalVC: UIViewController, UITextFieldDelegate {
         createGoalBtn.bindToKeyboard()
         pointsTxtField.delegate = self
     }
+    @IBAction func backBtnWasPressed(_ sender: Any) {
+        dismissDetail()
+    }
     @IBAction func createGoalBtnWasPressed(_ sender: Any) {
         // pass data to core data model
+        if pointsTxtField.text != nil {
+            self.save { (complete) in
+                if complete {
+                    dismiss(animated: true, completion: nil)
+                }
+            }
+        }
     }
     
+    func save(completion: (_ finished: Bool) -> ()) {
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
+        let goal = Goal(context: managedContext)
+        goal.goalDescription = goalDescription
+        goal.goalType = goalType.rawValue
+        goal.goalCompletionValue = Int32(pointsTxtField.text!)!
+        goal.goalProgress = Int32(0)
+        
+        do {
+           try managedContext.save()
+            print("Saved data to core data model")
+            completion(true)
+        } catch {
+            debugPrint("Could not save: \(error.localizedDescription)")
+            completion(false)
+        }
+        
+    }
 }
