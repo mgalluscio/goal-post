306471e8fdd234bac2993fa220b15f19a41b7102
Mario Galluscio <mgalluscio@deliverydudes.com>
2019-01-28 21:15:20 -0500

    added setProgress function to add days to goals when day is completed. Added to table view.

--- goal-post/goal-post/Views/GoalCell.swift
+++ goal-post/goal-post/Views/GoalCell.swift
@@ -12,10 +12,17 @@ class GoalCell: UITableViewCell {
     @IBOutlet weak var goalDescription: UILabel!
     @IBOutlet weak var goalType: UILabel!
     @IBOutlet weak var goalProgress: UILabel!
+    @IBOutlet weak var completionView: UIView!
     
     func configureCell(goal: Goal) {
         self.goalDescription.text = goal.goalDescription
         self.goalType.text = goal.goalType
-        self.goalProgress.text = String(describing: goal.goalCompletionValue)
+        self.goalProgress.text = String(describing: goal.goalProgress)
+        
+        if goal.goalProgress == goal.goalCompletionValue {
+            self.completionView.isHidden = false
+        } else {
+            self.completionView.isHidden = true
+        }
     }
 }
